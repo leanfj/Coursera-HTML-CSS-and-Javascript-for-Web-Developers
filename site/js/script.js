33bef8a2437b553caf4500960c3825eaea9b8075
Leandro Ferreira de Jesus <leanfj@hotmail.com>
2017-02-25 19:39:37 -0300

    Inclusão de conteudo dinamico utilizando Ajax apra inclusão do snippet home dentro da homepage

--- site/js/script.js
+++ site/js/script.js
@@ -6,4 +6,40 @@ $(function () {
 			$("#collapsable-nav").collapse('hide');
 		}
 	});
+	  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
 });
+
+// IIFE para inserir o snippet
+(function (global) {
+	var dc = {}; /*namespaces*/
+	var homeHtml = "snippets/home-snippet.html";
+
+	var insertHtml = function (selector, html) {
+		var targetElem = document.querySelector(selector);
+		targetElem.innerHTML = html;
+	};
+
+
+	// mostrar o icone de loading
+	var showLoading = function (selector) {
+		var html = "<div class='text-center'>";
+		html += "<img src='images/ajax-loader.gif'></div>";
+		insertHtml(selector, html);
+	};
+	// execução de conteudo apos o carregamento da pagina
+	document.addEventListener("DOMContentLoaded", function (event) {
+		showLoading("#main-content");
+		$ajaxTest.sendGetRequest(
+			homeHtml,
+			function (responseText) {
+				document.querySelector("#main-content").innerHTML = responseText;
+			},
+			false);
+	});
+
+global.$dc = dc;
+
+
+})(window);
