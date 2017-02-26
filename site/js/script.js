125fa4171027b0f0807c569800700331bfe815bd
Leandro Ferreira de Jesus <leanfj@hotmail.com>
2017-02-25 21:15:28 -0300

    Inclusão da requisição ajax para incluir a pagina de menu categories dinamicamente realicionada com o arquivo json externo.

--- site/js/script.js
+++ site/js/script.js
@@ -15,6 +15,9 @@ $(function () {
 (function (global) {
 	var dc = {}; /*namespaces*/
 	var homeHtml = "snippets/home-snippet.html";
+	var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
+	var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+	var categoryHtml = "snippets/category-snippet.html";
 
 	var insertHtml = function (selector, html) {
 		var targetElem = document.querySelector(selector);
@@ -28,6 +31,13 @@ $(function () {
 		html += "<img src='images/ajax-loader.gif'></div>";
 		insertHtml(selector, html);
 	};
+
+	var insertProprety = function (string, propName, propValue) {
+		var propToReplace = "{{" + propName + "}}";
+			string = string.replace(new RegExp(propToReplace, "g"), propValue);
+		return string;
+	}
+
 	// execução de conteudo apos o carregamento da pagina
 	document.addEventListener("DOMContentLoaded", function (event) {
 		showLoading("#main-content");
@@ -39,6 +49,48 @@ $(function () {
 			false);
 	});
 
+	dc.loadMenuCategories = function () {
+		showLoading("#main-content");
+		$ajaxTest.sendGetRequest(
+			allCategoriesUrl,
+			buildAndShowCategoriesHTML);
+	};
+
+	function buildAndShowCategoriesHTML (categories) {
+		$ajaxTest.sendGetRequest(
+			categoriesTitleHtml,
+			function (categoriesTitleHtml) {
+				$ajaxTest.sendGetRequest(
+					categoryHtml,
+					function (categoryHtml) {
+						var categoriesViewHtml = 
+							buildCategoriesViewHtml(
+								categories,
+								categoriesTitleHtml,
+								categoryHtml);
+						insertHtml("#main-content", categoriesViewHtml);
+					},
+					false);
+			},
+			false);
+	}
+	function buildCategoriesViewHtml (categories,categoriesTitleHtml, categoryHtml) {
+		var finalHtml = categoriesTitleHtml;
+		finalHtml += "<section class='row'>";
+
+		for (var i = 0; i < categories.length; i++) {
+			var html = categoryHtml;
+			var name = "" + categories[i].name;
+			var short_name = categories[i].short_name;
+			html = insertProprety(html, "name", name);
+			html = insertProprety(html, "short_name", short_name);
+			finalHtml += html;
+		}
+
+		finalHtml +="</section>";
+		return finalHtml;
+	}
+	
 global.$dc = dc;
 
 
